60f8c3a5a0561300630f0e274575c1c0e74a91c6
zuowei8 <zuowei8@tom.com>
2022-12-19 16:19:50 +0800

    update kernel code

---
+++ linux-mt/arch/arm64/include/generated/asm/cpucaps.h
@@ -0,0 +1,73 @@
+#ifndef __ASM_CPUCAPS_H
+#define __ASM_CPUCAPS_H
+
+/* Generated file - do not edit */
+
+#define ARM64_BTI                           	0
+#define ARM64_HAS_32BIT_EL0_DO_NOT_USE      	1
+#define ARM64_HAS_32BIT_EL1                 	2
+#define ARM64_HAS_ADDRESS_AUTH              	3
+#define ARM64_HAS_ADDRESS_AUTH_ARCH         	4
+#define ARM64_HAS_ADDRESS_AUTH_IMP_DEF      	5
+#define ARM64_HAS_AMU_EXTN                  	6
+#define ARM64_HAS_ARMv8_4_TTL               	7
+#define ARM64_HAS_CACHE_DIC                 	8
+#define ARM64_HAS_CACHE_IDC                 	9
+#define ARM64_HAS_CNP                       	10
+#define ARM64_HAS_CRC32                     	11
+#define ARM64_HAS_DCPODP                    	12
+#define ARM64_HAS_DCPOP                     	13
+#define ARM64_HAS_E0PD                      	14
+#define ARM64_HAS_EPAN                      	15
+#define ARM64_HAS_GENERIC_AUTH              	16
+#define ARM64_HAS_GENERIC_AUTH_ARCH         	17
+#define ARM64_HAS_GENERIC_AUTH_IMP_DEF      	18
+#define ARM64_HAS_IRQ_PRIO_MASKING          	19
+#define ARM64_HAS_LDAPR                     	20
+#define ARM64_HAS_LSE_ATOMICS               	21
+#define ARM64_HAS_NO_FPSIMD                 	22
+#define ARM64_HAS_NO_HW_PREFETCH            	23
+#define ARM64_HAS_PAN                       	24
+#define ARM64_HAS_RAS_EXTN                  	25
+#define ARM64_HAS_RNG                       	26
+#define ARM64_HAS_SB                        	27
+#define ARM64_HAS_STAGE2_FWB                	28
+#define ARM64_HAS_SYSREG_GIC_CPUIF          	29
+#define ARM64_HAS_TLB_RANGE                 	30
+#define ARM64_HAS_VIRT_HOST_EXTN            	31
+#define ARM64_HW_AF                         	32
+#define ARM64_HW_DBM                        	33
+#define ARM64_KVM_PROTECTED_MODE            	34
+#define ARM64_MISMATCHED_CACHE_TYPE         	35
+#define ARM64_MTE                           	36
+#define ARM64_SPECTRE_V2                    	37
+#define ARM64_SPECTRE_V3A                   	38
+#define ARM64_SPECTRE_V4                    	39
+#define ARM64_SPECTRE_BHB                   	40
+#define ARM64_SSBS                          	41
+#define ARM64_SVE                           	42
+#define ARM64_UNMAP_KERNEL_AT_EL0           	43
+#define ARM64_WORKAROUND_834220             	44
+#define ARM64_WORKAROUND_843419             	45
+#define ARM64_WORKAROUND_845719             	46
+#define ARM64_WORKAROUND_858921             	47
+#define ARM64_WORKAROUND_1418040            	48
+#define ARM64_WORKAROUND_1463225            	49
+#define ARM64_WORKAROUND_1508412            	50
+#define ARM64_WORKAROUND_1542419            	51
+#define ARM64_WORKAROUND_1742098            	52
+#define ARM64_WORKAROUND_2457168            	53
+#define ARM64_WORKAROUND_CAVIUM_23154       	54
+#define ARM64_WORKAROUND_CAVIUM_27456       	55
+#define ARM64_WORKAROUND_CAVIUM_30115       	56
+#define ARM64_WORKAROUND_CAVIUM_TX2_219_PRFM	57
+#define ARM64_WORKAROUND_CAVIUM_TX2_219_TVM 	58
+#define ARM64_WORKAROUND_CLEAN_CACHE        	59
+#define ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE	60
+#define ARM64_WORKAROUND_NVIDIA_CARMEL_CNP  	61
+#define ARM64_WORKAROUND_QCOM_FALKOR_E1003  	62
+#define ARM64_WORKAROUND_REPEAT_TLBI        	63
+#define ARM64_WORKAROUND_SPECULATIVE_AT     	64
+#define ARM64_NCAPS				65
+
+#endif /* __ASM_CPUCAPS_H */
